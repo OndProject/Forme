e4f8ec3ddea4f0eaa55eb7f24b9bce5ee37fe2af
y8mi <yoomi843@naver.com>
2023-01-27 18:12:38 +0900

    최종본

--- TotalProject/src/main/java/org/zerock/controller/DetailController.java
+++ TotalProject/src/main/java/org/zerock/controller/DetailController.java
@@ -144,5 +144,9 @@ public class DetailController {
 		return "sellerpostingcheck";
 	}
 	
+	@RequestMapping("/sellerbuyercheck")
+	public String sellerbuyercheck() {
+		return "sellerbuyercheck";
+	}
 	
 };
